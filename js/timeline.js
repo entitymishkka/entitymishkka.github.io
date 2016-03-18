01bc680813ccbed3ccf082d95229b9304cfc0c84
Tom <tommysmall92@gmail.com>
2016-03-18 04:12:22 +0000

    updates

--- js/timeline.js
+++ js/timeline.js
@@ -13,11 +13,10 @@ initScrollAnimations();
 function initScrollAnimations() {
   $('#content-wrapper').css('display','block');
   var controller = $.superscrollorama();
- 
-  controller.pin($('#scene1'), 17000, {
+  controller.pin($('#scene1'), 19500, {
                   anim: (new TimelineLite())
 				  	.append(
-                   TweenMax.from($('#welcome'), 1, 
+                   TweenMax.from($('#welcome'), 0.7, 
                                     {css:{opacity:1, top: 90, scaleX: 1, scaleY: 1}, immediateRender:true}, 
                                     {css:{opacity: 0, top: 60, scaleX: 0, scaleY: 0}}),
 									-2
@@ -42,12 +41,11 @@ function initScrollAnimations() {
 							-0.8
 									
                    )
-
 				   .append(
-                   TweenMax.fromTo($('#sky'), 1, 
+                   TweenMax.fromTo($('#sky'), 0.8, 
                                     {css:{top: -8500}, immediateRender:true}, 
                                     {css:{top: 0}}),
-									-1.6
+									-1.4
                    )
 				   .append(
                    TweenMax.fromTo($('#move-mid'), 1, 
@@ -63,44 +61,44 @@ function initScrollAnimations() {
 									-1
 					)
 					.append(
+                   TweenMax.fromTo($('#move-backg'), 1.45, 
+                                    {css:{bottom: -210}, immediateRender:true}, 
+                                    {css:{bottom: -850}}),
+									-1.1
+					)
+					.append(
                    TweenMax.from($('#introtext'), 1, 
                                     {css:{opacity:0.9, 'letter-spacing':'10px'}, immediateRender:true}, 
                                     {css:{opacity:0, 'letter-spacing':'40px'}})
-					
 					)
-
 				   .append(
                    TweenMax.fromTo($('#fling-it-right'), 1, 
                                     {css:{left:840, top: 200, scaleX: 1, scaleY: 1}, immediateRender:true}, 
-                                    {css:{left:190, top: 360, scaleX: 2.7, scaleY: 2.7}})
-							
-									
+                                    {css:{left:160, top: 390, scaleX: 2.7, scaleY: 2.7}})	
                    )
                    .append(
                    TweenMax.fromTo($('#fling-it'), 1, 
                                     {css:{left:100, top: 20, scaleX: 1, scaleY: 1}, immediateRender:true}, 
-                                    {css:{left:640, top: 10, scaleX: 0.4, scaleY: 0.4}}),
-									-1
-									
+                                    {css:{left:640, top: 5, scaleX: 0.4, scaleY: 0.4}}),
+									-1		
                    )
 				   .append(
                    TweenMax.fromTo($('#stars'), 1.7, 
                                     {css:{top: -280, opacity: 0}, immediateRender:true}, 
                                     {css:{top: 0, opacity: 1}}),
-									-0.9
-																		
+									-0.9													
                    )
 				.append(
-                   TweenMax.fromTo($('#belt'), 2, 
+                   TweenMax.fromTo($('#belt'), 1.7, 
                                     {css:{opacity: 0}, immediateRender:true}, 
-                                    {css:{opacity: 0.7}}),
-									0.5																		
+                                    {css:{opacity: 0.8}}),
+										-0.2
                    )
 				 .append(
-                   TweenMax.fromTo($('#infobox'), 1.1, 
+                   TweenMax.fromTo($('#infobox'), 1, 
                                     {css:{opacity: 0, bottom: -250}, immediateRender:true}, 
                                     {css:{opacity: 1, bottom: 50}}),
-									-1															
+									-0.3														
                    )
 				.append(
                    TweenMax.fromTo($('#fling-it-right'), 1, 
@@ -118,8 +116,7 @@ function initScrollAnimations() {
                    TweenMax.fromTo($('#infobox'), 0.9, 
                                     {css:{bottom: 50}, immediateRender:true}, 
                                     {css:{bottom: -250}}),
-									-1.4
-																							
+									-1.4																
                    )
                   })
   
